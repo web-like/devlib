d95aed05ec3e9c01cbf6c339e13c3d4295fe7779
wanglk <2885165573@qq.com>
2018-09-07 10:04:38 +0800

    解决冲突

--- maps/maps.go
+++ maps/maps.go
@@ -3,22 +3,25 @@ package maps
 import (
 	"strconv"
 	"github.com/web-like/devlib/structs"
-)
+		)
 
 type maps struct {
 }
 
 // 获取两个相同结构的map的值的异同  以第一个参数为基准
-func Diff(old interface{}, new interface{}) (map[string]string, []string) {
+func Diff(old interface{}, new interface{}, ignore... string) (map[string]string, []string) {
 
+	ignore = append(ignore, "Id")
 	data := make(map[string]string)
 	var keys []string
 
 	fields := structs.Names(old)
-	em := structs.Map(new)
+	em := structs.Map(old)
 	nm := structs.Map(new)
 	for _, v := range fields{
-		if v == "Id" || em[v] == nm[v] {
+		index := SliceExist(ignore, v)
+
+		if index != len(ignore) || em[v] == nm[v] {
 			continue
 		}
 
@@ -28,7 +31,7 @@ func Diff(old interface{}, new interface{}) (map[string]string, []string) {
 			keys = append(keys, v)
 			break
 		case int64:
-			data[v] = strconv.Itoa(int(em[v].(int))) + "###" + strconv.Itoa(int(nm[v].(int)))
+			data[v] = strconv.Itoa(int(em[v].(int64))) + "###" + strconv.Itoa(int(nm[v].(int64)))
 			keys = append(keys, v)
 			break
 		default:
@@ -36,10 +39,27 @@ func Diff(old interface{}, new interface{}) (map[string]string, []string) {
 			keys = append(keys, v)
 			break
 		}
+
 	}
 	return data, keys
 }
 
+
+func SliceExist(s []string, key string) (i int) {
+
+	num := len(s)
+
+	if num == 0 {
+		return 0
+	}
+
+	for k, v := range s{
+		if key == v {
+			return k
+		}
+	}
+	return  num
+}
 
 
 
